799d349c5d6b38bbc5d6747bc4eefb2a094a00af
xenoney <35218544+xenoney@users.noreply.github.com>
2019-09-16 16:22:54 +0900

    Create user registration function with Firebase

--- app/src/main/java/com/example/firebaseauthtutorial/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseauthtutorial/RegisterActivity.kt
@@ -3,16 +3,67 @@ package com.example.firebaseauthtutorial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import kotlinx.android.synthetic.main.activity_register.*
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        text_view_login.setOnClickListener {
-            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+        mAuth = FirebaseAuth.getInstance()
+
+        button_register.setOnClickListener {
+            val email = text_email.text.toString().trim()
+            val password = edit_text_password.text.toString()
+
+            if(email.isEmpty()) {
+                text_email.error = "Email required"
+                text_email.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                text_email.error = "Valid email required"
+                text_email.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty() || password.length < 6) {
+                edit_text_password.error = "6 char password required"
+                edit_text_password.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerUser(email, password)
         }
+
+        text_view_login.setOnClickListener {
+                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            }
+        }
+
+    private fun registerUser(email: String, password: String) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if(task.isSuccessful) {
+                val intent = Intent(this@RegisterActivity, HomeActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+
+                startActivity(intent)
+            }
+            else{
+                task.exception?.message?.let{
+                    toast(it)
+                }
+            }
+        }
+
     }
 }
